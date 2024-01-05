f892ffce03ec60221cd731b91bb67dc8f7f258d9
Doka <83892184+Dokaoista@users.noreply.github.com>
2024-01-05 09:23:34 -0300

    Adicionado a Categoria de Ferramentas Para Bug Bounty
    Foi feito a remoção da categoria "extras" e adicionado a de Bug Bounty. 
    Junto a isto, o arquivo "Menu.py" foi alterado para adaptar as modificações feitas no "categorias.py".

--- categorias.py
+++ categorias.py
@@ -4,14 +4,15 @@ categorias = {
 	3: ['Ataques Wireless', ['kali-tools-wireless']],
 	4: ['Aplicações Web', ['kali-tools-web']],
 	5: ['Sniffing e Spoofing', ['kali-tools-sniffing-spoofing']],
-	7: ['Ferramentas de Relatório', ['kali-tools-reporting']],
-	8: ['Ferramentas de Exploração', ['kali-tools-exploitation']],
-	9: ['Ferramentas Forenses', ['kali-tools-forensics']],
-	10: ['Teste de Estresse', ['dhcpig', 'funkload', 'iaxflood', 'inviteflood', 'ipv6-toolkit', 'mdk3', 'reaver', 'rtpflood', 'slowhttptest', 't50', 'termineter', 'thc-ipv6', 'thc-ssl-dos']],
-	11: ['Ataques de Senha', ['kali-tools-passwords']],
-	12: ['Engenharia Reversa', ['kali-tools-reverse-engineering']],
-	13: ['Hacking de Hardware', ['kali-tools-hardware']],
-    14: ['Ferramentas de Pós Exploração', ['kali-tools-post-exploitation']],
+	6: ['Ferramentas de Relatório', ['kali-tools-reporting']],
+	7: ['Ferramentas de Exploração', ['kali-tools-exploitation']],
+	8: ['Ferramentas Forenses', ['kali-tools-forensics']],
+	9: ['Teste de Estresse', ['dhcpig', 'funkload', 'iaxflood', 'inviteflood', 'ipv6-toolkit', 'mdk3', 'reaver', 'rtpflood', 'slowhttptest', 't50', 'termineter', 'thc-ipv6', 'thc-ssl-dos']],
+	10: ['Ataques de Senha', ['kali-tools-passwords']],
+	11: ['Engenharia Reversa', ['kali-tools-reverse-engineering']],
+	12: ['Hacking de Hardware', ['kali-tools-hardware']],
+    13: ['Ferramentas de Pós Exploração', ['kali-tools-post-exploitation']],
+    14: ['Ferramentas de Bug Bounty', ['FFF,airixss,Freq,Goop,Hakrawler,Httprobe,Meg,Haklistgen,Haktldextract,Hakcheckurl,tojson,gowitness,rush,hakcheckurl,shuffledns,rescope,gron,html-tool,gf,qsreplace,Amass,ffuf,assetfinder,github-subdomains,cf-check,waybackurls,nuclei,anew,notify,mildew,dirdar,unfurl,shuffledns,httpx,github-endpoints,dnsx,subfinder,gauplus,subjs,subjs,Gxss,crobat,dalfox,puredns,cariddi,interactsh-client,kxss,GetJs,hakrevdns,dnsgen']]
 }
 
 categorias[15] = ['Todas as Ferramentas', [ferramenta for ferramentas in categorias.values() for ferramenta in ferramentas[1]]] # Adicição de todas as ferramentas para opção de instalação
